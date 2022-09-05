327ccdf1abaeac5a7920557e00f355f20df6ad2a
sreeja <gaddameedisreeja74@gmail.com>
2022-09-05 01:21:42 -0700

    FINAL COMMIT

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar(props) {
     <div>
     <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
         <div className="container">
-          <NavLink className="navbar-brand fw-bold fs-4" to="/">Navbar</NavLink>
+          <NavLink className="navbar-brand fw-bold fs-4" to="/">Emart</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
